3bafc51b2e0bbaa126031f2fc41c7c80ba503334
pbiswasCSE <pbiswascse@gmail.com>
2024-11-27 18:54:07 +0600

    Initial Setup

---
+++ src/main/java/com/directmart/directmart/DirectmartApplication.java
@@ -0,0 +1,13 @@
+package com.directmart.directmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/directmart/directmart/DirectmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.directmart.directmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
